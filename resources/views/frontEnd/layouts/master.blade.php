7c5a807329f83db308a45142e03892517288c95d
Shadin Sarkar <sadhinsharkar564@gmail.com>
2025-12-06 15:46:40 +0600

    gsdfg

--- resources/views/frontEnd/layouts/master.blade.php
+++ resources/views/frontEnd/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>@yield('title') - {{ $generalsetting->name }}</title>
+    <title>@yield('title') - {{ $generalsetting->name ?? null }}</title>
 
     <meta name="facebook-domain-verification" content="2f7vvnzbpug91a8t8gp4ei177bkf2x" />
     <!-- App favicon -->
